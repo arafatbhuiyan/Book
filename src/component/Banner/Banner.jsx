7aad51d920ef23f63ca905b715e5484a2df662c7
arafatbhuiyan <mdarafatbhuiyan06@gmail.com>
2026-04-23 13:09:56 +0600

    data load

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -2,24 +2,34 @@ import React from "react";
 import Bookss from "../../assets/Bookss.png";
 
 const Banner = () => {
+    const ButtonClick = ()=>{
+        alert("No Added")
+    }
   return (
-    <div className="bg-[#f1faee]">
-      <div className="flex max-w-6xl mx-auto">
-        <div>
-          <h1
-            className=" text-3xl md:text-4xl lg:text-5xl ml-4 md:ml-0 lg:ml-24 pt-10 md:pt-16 lg:pt-24 md:text-center lg:text-left font-bold">
+    <div className="bg-[#f1faee] py-2 md:py-20 lg:py-15">
+      <div className="flex flex-col lg:flex-row items-center justify-around max-w-6xl mx-auto px-4 lg:px-8">
 
-            Books to freshen up <br />{" "}
-            <span className="block mt-1">your bookshelf</span>
+        {/* Text Section */}
+        <div className="text-center lg:text-left order-2 lg:order-1">
+          <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold">
+            Books to freshen up <br />
+            <span className="block mt-2">your bookshelf</span>
           </h1>
-          <button className="border py-2 px-7 rounded bg-green-500 text-white ml-25 mt-5 cursor-pointer hover:bg-green-600">
+
+          <button onClick={ButtonClick} className="mt-6 py-2 px-7 rounded bg-green-500 text-white hover:bg-green-600">
             View the List
           </button>
         </div>
 
-        <div className="ml-45 h-40 w-50 mb-50 mt-18">
-          <img src={Bookss} alt="" />
+        {/* Image Section */}
+        <div className="mt-10 lg:mt-0 order-1 lg:order-2">
+          <img
+            src={Bookss}
+            alt="Books"
+            className="w-25 md:w-80 lg:w-40"
+          />
         </div>
+
       </div>
     </div>
   );
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Suspense, useState } from 'react';
+import Book from '../Book/Book';
 
-const Books = () => {
+const Books = ({data}) => {
+    const[allBooks, setAllBooks] =useState([])
+
+    // const bookPromis = fetch('./booksData.json').then(res => res.json())
+
     return (
         <div>
-           <h1>I am books pages</h1> 
+            <Suspense fallback={<h1>Loading.....</h1>}>
+            <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                data.map((singlebook)=><Book key={singlebook.bookId} singlebook={singlebook}></Book> )
+               }
+            </div>
+               
+            </Suspense>
+        
         </div>
     );
 };
